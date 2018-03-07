3dd200279271fbdd904f0a97b4fc4181e5271514
zuodengchao <983625108@qq.com>
2018-03-07 15:42:13 +0800

    宿舍

--- sss/src/sss/bbbbbbb.java
+++ sss/src/sss/bbbbbbb.java
@@ -1,5 +1,7 @@
 package sss;
 
 public class bbbbbbb {
-
+  public static void main(String[] args) {
+	  System.out.println();
+  }
 }
--- sss/src/sss/sb.java
+++ sss/src/sss/sb.java
@@ -4,5 +4,6 @@ public class sb {
   public static void main(String[] args) {
 	System.out.println("sb");
 	System.out.println("牛逼");
+	System.out.println();
    }
 }
